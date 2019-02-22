98c26cbbbdea3b9816c06781f49652a328907c2b
BenTrump <bmtseducation@gmail.com>
2019-02-21 16:00:27 -0800

    Added additional post check-in clicks. Fully functional

--- CheckIn.py
+++ CheckIn.py
@@ -1,12 +1,16 @@
-import selenium
+#  import selenium
 from selenium import webdriver
 import time
 
 
 def check_in():
-    confirmation_number = "FDEEY9"
+    confirmation_number = "ABCDE9"
     first_name = "Tom"
     last_name = "Brady"
+    email = "tom_brady@hotmail.com"
+    date_time = "date.time format"
+
+    # Todo: add a 'wait until' feature that allows user to run script all day until they can check in
 
     driver = webdriver.Chrome()
     driver.get("https://www.southwest.com/air/check-in/")
@@ -18,9 +22,25 @@ def check_in():
     first_name_box_id.send_keys(first_name)
     last_name_box_id.send_keys(last_name)
     checkin_button = driver.find_element_by_id("form-mixin--submit-button")
-    for i in range(5):
-        checkin_button.click()
-        time.sleep(3)
+    checkin_button.click()
+    time.sleep(3)
+
+    second_checkin_btn = driver.find_element_by_xpath('//*[@data-a="airCheckInReviewResults_checkIn"]')
+    second_checkin_btn.click()
+    time.sleep(2)
+
+    email_button = driver.find_element_by_xpath(
+        '//*[@data-a="airCheckInBoardingPassOption_button_mobileBoardingPassOption_email"]')
+    email_button.click()
+    time.sleep(2)
+
+    email_box_id = driver.find_element_by_id("emailBoardingPass")
+    email_box_id.send_keys(email)
+    time.sleep(2)
+
+    email_submit_btn = driver.find_element_by_id("form-mixin--submit-button")
+    email_submit_btn.click()
+    time.sleep(5)
 
 
 check_in()
